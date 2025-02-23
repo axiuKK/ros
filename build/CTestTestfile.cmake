3268ac83b1e14a734c1811277c78f9c182def99e
axiuKK <2931432685@qq.com>
2025-02-23 20:03:01 +0800

    rosLearn

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,13 @@
+# CMake generated Testfile for 
+# Source directory: /home/thy/catkin_ws/src
+# Build directory: /home/thy/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("atr_pkg")
+subdirs("ssr_pkg")
+subdirs("imu_pkg")
+subdirs("lidar_pkg")
+subdirs("vel_pkg")
+subdirs("wpr_simulation")
